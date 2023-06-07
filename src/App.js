923e53d11501df872dde00f6be5e6b25833c35bb
Willian Fernandes Vieira <113148195+willfevi@users.noreply.github.com>
2023-06-07 10:29:14 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+import api from './services/api'
 function App() {
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch() {
+    if (input == '') {
+      alert('Preencha com um Cep válido!')
+      return
+    }
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+    } catch {
+      alert('Erro ao fazer a consulta, verifique o cep e tente novamente!')
+      setInput('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1 className="Title">Busque o CEP:</h1>
+
+      <div className="Container_input">
+        <input
+          type="text"
+          placeholder="Digite seu CEP:"
+          value={input}
+          onChange={e => {
+            return setInput(e.target.value)
+          }}
+        ></input>
+
+        <button className="Search_button" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP:{cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span> complemento:{cep.complemento}</span>
+          <span>{cep.bairro} </span>
+          <span>
+            {cep.localidade}-{cep.uf}
+          </span>
+        </main>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
